ae63fd89a9e64128c62c518121a92d46c2c86cc8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-06-25 19:03:14 +0800

    manage subscribe channel in spark session

--- src/gen/gen-code.ts
+++ src/gen/gen-code.ts
@@ -224,7 +224,7 @@ export class ${controllerClassName} {
         newConnection(spark);
         spark.on('end', () => closeConnection(spark));
         spark.on('${callApiPath}', async (call: CallInput<${callTypeName}>, ack) => {
-          startSparkCall(spark.id, call);
+          startSparkCall(spark, call);
           try {
             await this.ready;
             await this.logService.storeObject(call);
@@ -239,7 +239,7 @@ export class ${controllerClassName} {
               message: e.message,
             });
           } finally {
-            endSparkCall(spark.id, call);
+            endSparkCall(spark, call);
           }
         });
       });
@@ -625,28 +625,56 @@ export function genConnectionCode(): string {
   return `
 import { CallInput } from 'cqrs-exp';
 
-export type Spark = {
-  id: string
-  on: (event: string, cb: (data: any, ack?: (data: any) => void) => void) => void
-} & any;
+export interface Spark {
+  id: string;
 
-export interface Session {
-  spark: Spark
-  calls: CallInput[]
+  on(event: string, cb: (data: any, ack?: (data: any) => void) => void);
+
+  send(event: string, data?: any, ack?: (data: any) => void);
 }
 
-export let sessions: Map<string, Session> = new Map();
+export interface Subscription {
+  id: string;
+
+  close();
+}
+
+export interface Session {
+  spark: Spark;
+  calls: CallInput[];
+  // channel id -> Subscription
+  subscriptions: Map<string, Subscription>;
+}
+
+let sparkId_session_map: Map<string, Session> = new Map();
+let in_session_map = new Map<any, Session>();
 
 export function newConnection(spark: Spark) {
-  sessions.set(spark.id, { spark, calls: [] });
+  sparkId_session_map.set(spark.id, {
+    spark,
+    calls: [],
+    subscriptions: new Map(),
+  });
 }
 
 export function closeConnection(spark: Spark) {
-  sessions.delete(spark.id);
+  let session = sparkId_session_map.get(spark.id);
+  session.subscriptions.forEach(sub => sub.close());
+  sparkId_session_map.delete(spark.id);
 }
 
-export function startSparkCall(sparkId: string, call: CallInput) {
-  sessions.get(sparkId).calls.push(call);
+export function startSparkCall(spark: Spark, call: CallInput) {
+  let session = sparkId_session_map.get(spark.id);
+  session.calls.push(call);
+  in_session_map.set(call.In, session);
+}
+
+export function getSessionByIn(In): Session {
+  return in_session_map.get(In);
+}
+
+export function getSessionBySparkId(sparkId: string): Session {
+  return sparkId_session_map.get(sparkId);
 }
 
 /**
@@ -660,9 +688,10 @@ function remove<A>(xs: A[], x: A): void {
   }
 }
 
-export function endSparkCall(sparkId: string, call: CallInput) {
-  const session = sessions.get(sparkId);
+export function endSparkCall(spark: Spark, call: CallInput) {
+  const session = sparkId_session_map.get(spark.id);
   remove(session.calls, call);
+  in_session_map.delete(call.In);
 }
 `.trim();
 }
