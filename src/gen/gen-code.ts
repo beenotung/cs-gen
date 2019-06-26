46793ff1c62c1b21b0d56629662f82839f029195
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-06-26 18:03:06 +0800

    make primus not exported

--- src/gen/gen-code.ts
+++ src/gen/gen-code.ts
@@ -400,7 +400,7 @@ export function genMainCode(originalCode: string): string {
     `import { Server } from 'http';
 
 const Primus = require('primus');
-export let primus;
+let primus;
 
 let pfs: Array<(primus) => void> = [];
 
