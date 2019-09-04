b9fdd092789a7b01be256c2b50e1f0c8e762560b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-09-05 00:22:42 +0800

    finished cli of mode: init, gen, help

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
-import { hasFile, readFile, writeFile } from '@beenotung/tslib/fs';
-import { catchMain } from '@beenotung/tslib/node';
+import { spawn } from '@beenotung/tslib/child_process';
+import {
+  hasDirectory,
+  hasFile,
+  readdir,
+  readFile,
+  writeFile,
+} from '@beenotung/tslib/fs';
 import * as path from 'path';
 import * as readline from 'readline';
+import { ReadLine } from 'readline';
 // @ts-ignore
 const mkdirp = require('async-mkdirp');
+const hasbin = require('hasbin');
 
-const io = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+let io: ReadLine | undefined;
 
 async function initPackageJson(name: string) {
   let packageJson: any = {};
@@ -23,7 +28,7 @@ async function initPackageJson(name: string) {
   };
   packageJson.dependencies = {
     ...packageJson.dependencies,
-    '@beenotung/tslib': '^14.34.1',
+    '@beenotung/tslib': '^14.35.0',
     'cqrs-exp': path.join(
       process.env.HOME!,
       'workspace',
@@ -104,8 +109,14 @@ catchMain(genProject({
 }
 
 async function ask(name: string, defaultAnswer: string): Promise<string> {
+  if (!io) {
+    io = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
   return new Promise<string>((resolve, reject) => {
-    io.question(`${name} [${defaultAnswer}]: `, answer => {
+    io!.question(`${name} [${defaultAnswer}]: `, answer => {
       if (answer) {
         resolve(answer);
       } else {
@@ -119,12 +130,17 @@ async function initProject() {
   const cwd = path.basename(process.cwd());
   const name = await ask('project name', cwd);
   const serverDomain = await ask('server domain', 'example.com');
-  const appId = await ask('app id', `${name}.${serverDomain}`);
   const serverOrigin = await ask(
     'server origin',
     `https://${name}.${serverDomain}`,
   );
-  io.close();
+  const appId = await ask(
+    'app id',
+    `${name}.${serverDomain}`
+      .split('.')
+      .reverse()
+      .join('.'),
+  );
   console.log(`initializing gen-project for '${name}'`);
   if (name !== cwd) {
     await mkdirp(name);
@@ -145,4 +161,78 @@ async function initProject() {
   console.log('generated skeleton.');
 }
 
-catchMain(initProject());
+function help() {
+  console.log(
+    `
+cqrs-exp [command]
+Commands:
+  init  : initialize project skeleton, create setup package.json and scripts/gen-project.ts
+  gen   : generate the client-side SDK for -client and -admin project, and create stub code for -server project
+  help  : show this help message
+`.trim(),
+  );
+}
+
+function call(cmd: string) {
+  return spawn({ cmd });
+}
+
+async function gen() {
+  let name: string | undefined;
+  if (await hasFile('package.json')) {
+    name = JSON.parse((await readFile('package.json')).toString()).name;
+  }
+  if (!name) {
+    const files = await readdir('.');
+    files: for (const file of files) {
+      for (const part of ['client', 'admin', 'server']) {
+        if (file.endsWith('-' + part)) {
+          name = file.replace('-' + part, '');
+          break files;
+        }
+      }
+    }
+  }
+  if (!name) {
+    console.error('project name not detected');
+    process.exit(1);
+  }
+  if (!hasbin.sync('pnpm')) {
+    await call('npm i -g pnpm');
+  }
+  await call('npm run gen');
+  process.chdir('scripts');
+  for (const part of ['server', 'client', 'admin']) {
+    process.chdir(path.join('..', `${name}-${part}`));
+    if (!(await hasDirectory('node_modules'))) {
+      await call('pnpm i');
+    }
+    await call('npm run format');
+  }
+}
+
+async function main() {
+  try {
+    const mode = process.argv[2];
+    switch (mode) {
+      case 'init':
+        await initProject();
+        break;
+      case 'gen':
+        await gen();
+        break;
+      case 'help':
+      default:
+        help();
+    }
+  } catch (e) {
+    console.error(e);
+    process.exit(0);
+  } finally {
+    if (io) {
+      io.close();
+    }
+  }
+}
+
+main();
