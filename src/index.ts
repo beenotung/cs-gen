78cd91e86ebabe2f4d6eda21a681ab0fa39b0888
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-01-31 02:04:51 +0800

    exclude log.service from cqrs-exp dist

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './types';
 export * from './utils';
 export * from './utils/reflect';
 export * from './utils/version';
-export * from './log/log.service';
+// export * from './log/log.service';
 export * from './store/types';
 export * from './store/db';
 export * from './gen/gen-file';
