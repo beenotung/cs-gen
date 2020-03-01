248a81ad1e6418ff3d65b7b4a021897be07198fc
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-03-01 21:55:22 +0800

    only call prebuild script if exist

--- src/gen/gen-file.ts
+++ src/gen/gen-file.ts
@@ -607,7 +607,11 @@ async function setServerPackage(args: {
   devDep.ctsc = '^1.1.0';
   const build = 'ctsc';
   if (json.scripts.build && json.scripts.build !== build) {
-    json.scripts['build:nest'] = 'npm run prebuild && ' + json.scripts.build;
+    let backup = json.scripts.build;
+    if (json.scripts.prebuild) {
+      backup = 'npm run prebuild && ' + backup;
+    }
+    json.scripts['build:nest'] = backup;
   }
   json.scripts.build = build;
   json.scripts.pm2 = `pm2 restart ${baseProjectName} || pm2 start --name ${baseProjectName} dist/main.js`;
