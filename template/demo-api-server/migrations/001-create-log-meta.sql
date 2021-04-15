7f7da9db71d6913f8b62a88c5bf619f02a5e3d69
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-04-16 06:51:01 +0800

    make log_meta id not auto increment; add acc to log to distinct same-time log ordering;

--- template/demo-api-server/migrations/001-create-log-meta.sql
+++ template/demo-api-server/migrations/001-create-log-meta.sql
@@ -1,6 +1,6 @@
 -- Up
 create table if not exists log_meta (
-    id integer primary key,
+    id integer not null unique,
     call_type text not null, -- e.g. Command, Query, Subscribe
     type text not null unique,
     replay integer not null -- bool
@@ -9,6 +9,7 @@ create table if not exists log_meta (
 create table if not exists log (
     id integer primary key,
     timestamp integer not null,
+    acc integer not null, -- to distinct the order of same-time logs
     meta_id integer not null references log_meta(id)
 );
 
