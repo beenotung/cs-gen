1012fd66fabbcedd0cae4a22407e843f9d719079
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-04-18 05:16:19 +0800

    restore for mixed files in last merge

--- template/demo-api/config/call-meta.ts
+++ template/demo-api/config/call-meta.ts
@@ -14,6 +14,15 @@ export const callMetas: CallMeta[] = [
   },
   {
     id: 2,
+    call_type: 'query',
+    type: 'get_all_usernames',
+    replay: false,
+    out: {
+      usernames: ['text', 'string[]'],
+    },
+  },
+  {
+    id: 3,
     call_type: 'command',
     type: 'change_username',
     replay: true,
@@ -24,7 +33,7 @@ export const callMetas: CallMeta[] = [
     errors: ['username already used'],
   },
   {
-    id: 3,
+    id: 4,
     call_type: 'query',
     type: 'check_username_exist',
     replay: false,
@@ -36,43 +45,62 @@ export const callMetas: CallMeta[] = [
     },
   },
   {
-    id: 4,
-    call_type: 'query',
-    type: 'get_all_username',
-    replay: false,
-    out: {
-      usernames: ['text', 'string[]'],
-    },
-  },
-  {
     id: 5,
     call_type: 'command',
-    type: 'report_stats',
-    replay: false,
+    type: 'delete_username',
+    replay: true,
     in: {
-      appCodeName: 'text', // e.g. Mozilla
-      appName: 'text', // e.g. Netscape
-      appVersion: 'text', // e.g. 5.0 (Windows)
-      language: 'text', // e.g. en-US
-      languages: ['text', 'string[]'], // e.g. ['en-US', 'en']
-      oscpu: 'text', // e.g. Windows NT 10.0; Win64; x64
-      platform: 'text', // e.g. Win32
-      product: 'text', // e.g. Gecko
-      userAgent: 'text', // e.g. Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0
-      vendor: 'text', // e.g. Google Inc.
+      username: 'text',
     },
+    errors: ['username is not used'],
   },
   {
     id: 6,
-    call_type: 'subscribe',
-    type: 'subscribe_users',
+    call_type: 'command',
+    type: 'log_browser_stats',
     replay: false,
-    out: {
-      feed_id: 'text',
+    in: {
+      'userAgent?': 'text', // e.g. Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0
+      'language?': 'text', // e.g. en-US
+      'languages?': ['text', 'string[]'], // e.g. ['en-US', 'en']
+      'deviceMemory?': 'integer',
+      'hardwareConcurrency?': 'integer',
+      'maxTouchPoints?': 'integer',
+      'platform?': 'text', // e.g. Win32
+      'vendor?': 'text', // e.g. Google Inc.
+      'connection?': [
+        'text',
+        `{
+  downlink: number
+  effectiveType: string
+  saveData: boolean
+}`,
+      ],
+      'cookieEnabled?': ['integer', 'boolean'],
     },
   },
   {
     id: 7,
+    call_type: 'subscribe',
+    type: 'subscribe_username',
+    replay: false,
+    out: {
+      feed_id: 'text',
+    },
+    feed: {
+      new_username: 'text',
+      del_username: 'text',
+      change_username: [
+        'text',
+        `{
+  from_username: string
+  to_username: string
+}`,
+      ],
+    },
+  },
+  {
+    id: 8,
     call_type: 'command',
     type: 'cancel_subscribe',
     replay: false,
--- template/demo-api/helpers/types.ts
+++ template/demo-api/helpers/types.ts
@@ -1,28 +1,33 @@
 export type CallMeta = {
-  id: number // unique
+  id: number // should be unique
   call_type: 'command' | 'query' | 'subscribe'
   type: string
+  replay: boolean
   in?: ObjectType
   out?: ObjectType
-  errors?: string[] // error message failed
-  feed?: ObjectType // default is not food
-  replay: boolean
+  errors?: string[] // error message when failed
+  feed?: ObjectType // default is no feed
   async?: boolean // default is sync
 }
 
 export type ObjectType = Record<string, FieldType>
 
-export type FieldType = sql_type | [sql_type, ts_type]
+export type FieldType = SqlType | [SqlType, TsType]
 
-export type ts_type = Reasons | string
+export type TsType = 'number' | 'string' | 'Buffer' | 'any' | 'boolean' | string
 
-export type Reasons = string[]
+export type SqlType = 'integer' | 'real' | 'text' | 'blob'
 
-export type sql_type = 'integer' | 'real' | 'text' | 'blob'
-
-export const sql_to_ts = {
+const sqlTypeToTsType: Record<SqlType, TsType> = {
   integer: 'number',
   real: 'number',
   text: 'string',
   blob: 'Buffer',
+}
+
+export function toTsFieldType(type: FieldType): TsType {
+  if (Array.isArray(type)) {
+    return type[1]
+  }
+  return sqlTypeToTsType[type] || 'any'
 }
