d334c9deb54a028573a8f9e4c4d83b230d797d68
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-04-27 03:06:07 +0800

    use path.join to in gen-sql-insert.ts to be platform independent

--- template/demo-api/scripts/gen-sql-insert.ts
+++ template/demo-api/scripts/gen-sql-insert.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env ts-node
+import { join } from 'path'
 import { genInsertFile } from '../helpers/gen-sql-insert'
 import { callMetas } from '../config/call-meta'
 
 genInsertFile(
   callMetas,
-  'src/insert-logs.ts',
+  join('src', 'insert-logs.ts'),
   `import { db } from '../config/db'`,
 )
