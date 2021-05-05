1add05f4a7448b5d90217c828598d82a850d1c85
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-05 20:14:53 +0800

    merge ids.ts into types.ts

--- template/demo-api/src/types.ts
+++ template/demo-api/src/types.ts
@@ -155,4 +155,15 @@ export type CallOut =
   | Result<SubscribeUsername>
   | Result<CancelSubscribe>
 
+export const ids = {
+  create_user: 1 as const,
+  get_all_usernames: 2 as const,
+  change_username: 3 as const,
+  check_username_exist: 4 as const,
+  delete_username: 5 as const,
+  log_browser_stats: 6 as const,
+  subscribe_username: 7 as const,
+  cancel_subscribe: 8 as const,
+}
+
 export const ok = { out: void 0 }
