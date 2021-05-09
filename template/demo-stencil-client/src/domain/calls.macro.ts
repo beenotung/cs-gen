856a15146051a43ce5d7fb81332a6a026544c935
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-09 14:20:26 +0800

    remove deprecated experimental template/demo with nestjs server and stencil client

--- template/demo-stencil-client/src/domain/calls.macro.ts
+++
@@ -1,6 +0,0 @@
-import { linesToCode, genCallTypes } from '../../../template/gen-code';
-import { apiConfig, calls } from '../../../demo-config/src/calls';
-
-let lines = genCallTypes(calls);
-lines.push(`export let apiConfig = ${JSON.stringify(apiConfig)}`);
-linesToCode(lines);
