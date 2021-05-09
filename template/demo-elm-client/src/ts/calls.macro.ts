d673d8d2447cca9aca577c8eccdde5fc62ed9a0c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-09 14:12:25 +0800

    remove demo-elm-client
    do not aim to auto-generate encoder and decoder at the moment

--- template/demo-elm-client/src/ts/calls.macro.ts
+++
@@ -1,7 +0,0 @@
-import { linesToCode } from '../../../template/gen-code'
-import { apiConfig, calls } from '../../../demo-config/src/calls'
-
-let lines: string[] = []
-lines.push(`export let calls = ${JSON.stringify(calls, null, 2)}`)
-lines.push(`export let apiConfig = ${JSON.stringify(apiConfig)}`)
-linesToCode(lines)
