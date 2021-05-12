cbc3f1c3afc53737a09cf8598b038eb38fbd7ea4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-13 00:26:06 +0800

    remove unused files - aggregate object, event type - engine, dispatcher - rethinkdb event store and command bus

--- src/store/types.ts
+++
@@ -1,9 +0,0 @@
-import { IEvent } from '../types';
-
-export interface EventSelector {
-  aggregate_id: IEvent['aggregate_id'];
-  skip?: number;
-  command_id?: IEvent['command_id'];
-  version?: IEvent['version'];
-  event_types?: Array<IEvent['event_type']>;
-}
