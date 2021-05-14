bcd0886996779114a6a9bee09ce8a16fd98b258a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-14 16:01:32 +0800

    remove example project generated from old approach

--- example/example-server/scripts/deduplicate-batch.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ts-node
-import { deduplicateBatch } from '../src/lib/batch';
-import { logService } from './utils';
-
-console.log('start deduplicate batch');
-deduplicateBatch(logService);
-console.log('finished deduplicate batch');
