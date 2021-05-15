3c8447eb6ef02898e4be82fcf68fbe5f667a2de1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-15 12:43:39 +0800

    remove old cqrs-exp files
    working to be monorepo with templates and npm-init-style create-* packages

--- src/gen/helpers/indent.ts
+++
@@ -1,6 +0,0 @@
-export function addIndentation(code: string, indentation: string): string {
-  return code
-    .split('\n')
-    .map(line => indentation + line)
-    .join('\n');
-}
--- src/gen/helpers/object.ts
+++
@@ -1,8 +0,0 @@
-export function sortObjectKey<T extends object>(json: T): T {
-  const src: any = json;
-  const res: any = {};
-  Object.keys(json)
-    .sort()
-    .forEach(key => (res[key] = src[key]));
-  return res;
-}
