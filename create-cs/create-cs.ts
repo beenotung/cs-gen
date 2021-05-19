9a71f498b6c8b68b07cd8cd0c6d323e14ccc1df1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-19 19:03:00 +0800

    extracted common flows in create-ts into npm-init-helper; auto set package name of new project

--- create-cs/create-cs.ts
+++ create-cs/create-cs.ts
@@ -1,50 +1,6 @@
 #!/usr/bin/env node
-import * as fs from 'fs'
-import * as readline from 'readline'
 import * as path from 'path'
-import degit from 'degit'
-
-async function clone(dest: string) {
-  let src = 'https://github.com/beenotung/cs-gen#template-macro'
-  console.log('Cloning from', src, '...')
-  let git = degit(src)
-  // git.on('info', info => console.log(info.message))
-  git.on('warn', info => console.warn(info.message))
-  await git.clone(dest)
-}
-
-async function getDest() {
-  let dest = process.argv[2]
-  if (!dest) {
-    let io = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    })
-    dest = await new Promise(resolve =>
-      io.question('project-directory: ', resolve),
-    )
-    io.close()
-  }
-  if (!dest) {
-    console.error('Please specify the project directory')
-    process.exit(1)
-  }
-  if (fs.existsSync(dest)) {
-    console.error('Error:', dest, 'already exists')
-    process.exit(1)
-  }
-  return dest
-}
-
-function setup(repoDir: string, dest: string) {
-  console.log('Creating a new command souring module in', dest, '...')
-  let src = path.join(repoDir, 'template', 'demo-server')
-  fs.renameSync(src, dest)
-}
-
-function cleanup(repoDir: string) {
-  fs.rmdirSync(repoDir, { recursive: true })
-}
+import { cloneTemplate, getDest } from 'npm-init-helper'
 
 function greet(dest: string) {
   console.log(
@@ -94,13 +50,13 @@ If you run into issues, you can lookup or report on https://github.com/beenotung
 
 async function main() {
   let dest = await getDest()
-  let repoDir = fs.mkdtempSync(dest + '.tmp')
-  await clone(repoDir)
-  try {
-    setup(repoDir, dest)
-  } finally {
-    cleanup(repoDir)
-  }
+  await cloneTemplate({
+    gitSrc: 'https://github.com/beenotung/cs-gen#template-macro',
+    srcDir: path.join('template', 'demo-server'),
+    dest,
+    showLog: true,
+    updatePackageJson: true,
+  })
   greet(dest)
 }
 
